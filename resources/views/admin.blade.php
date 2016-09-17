dc95724d32d9d174d9a491a96f43be7a9321bf6f
emilysf <emilysfrazin@gmail.com>
2016-09-17 13:06:49 -0400

    tables added

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,2 +1,85 @@
 @include ('nav-admin')
+@include ('header')
+
+<style>
+	.col-xs-11 {
+		margin-left: 4%;
+		margin-top: 3%;
+	}
+	form{
+    border-radius:25px;
+  }
+  .box {
+    border:2px solid #000059 !important;
+  }
+  td, th {
+    padding-left: 20px !important;
+  }
+</style>
+
+	<div class="row">
+        <div class="col-xs-6">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Class Schedule</h3>
+
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tr>
+                  <th>Instructor Name</th>
+                  <th>Instructor Age</th>
+                  <th>Instructor Status</th>
+                </tr>
+             	<tr>
+                  <td>Mary Ann</td>
+                  <td>27</td>
+                  <td>Full Time/Studio Director</td>
+                </tr>
+                
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <div class="col-xs-6">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Class Schedule</h3>
+
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tr>
+                  <th>Instructor Name</th>
+                  <th>Instructor Age</th>
+                  <th>Instructor Status</th>
+                </tr>
+             	<tr>
+                  <td>Mary Ann</td>
+                  <td>27</td>
+                  <td>Full Time/Studio Director</td>
+                </tr>
+                
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+    </div>
+
 
